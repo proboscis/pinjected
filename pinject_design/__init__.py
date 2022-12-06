1bd378fad59cbacf64ad809c701a809790e77e34
Kento Masui <nameissoap@gmail.com>
2022-12-06 15:35:42 +0900

    Now I realized both content/style transfer doesnt work in latent code!

--- pinject_design/__init__.py
+++ pinject_design/__init__.py
@@ -1,4 +1,4 @@
-from pinject_design.di.injected import Injected, injected_factory
+from pinject_design.di.injected import Injected, injected_function
 from pinject_design.di.util import Design,EmptyDesign
 from pinject_design.di.designed import Designed
 
