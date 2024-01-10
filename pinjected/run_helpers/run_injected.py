e0ee678798aa0906e42e3fdcf710e1d931512e06
proboscis <nameissoap@gmail.com>
2024-01-10 12:57:38 +0900

    Refactor run_injected functions for better type handling
    Improved the handling of Injected and Designed types within the pinjected functions. Injected types are now ensured to be injected while Designed types can encapsulate their design. Also relocated 'os' import statement for better code organization and reduces dependence.

--- pinjected/run_helpers/run_injected.py
+++ pinjected/run_helpers/run_injected.py
@@ -1,4 +1,5 @@
 import inspect
+import os
 from functools import wraps
 from pathlib import Path
 
@@ -11,13 +12,14 @@ from typing import Coroutine, Awaitable
 from loguru import logger
 from returns.result import safe
 
-from pinjected import instances, Injected, Design, providers
+from pinjected import instances, Injected, Design, providers, Designed
+from pinjected.di.proxiable import DelegatedVar
 from pinjected.helper_structure import MetaContext
 from pinjected.helpers import get_design_path_from_var_path
 from pinjected.module_var_path import ModuleVarPath, load_variable_by_module_path
 from pinjected.logging_helper import disable_internal_logging
 from pinjected.notification import notify
-from pinjected.run_helpers.pinjected_environments import load_user_default_design, load_user_overrides_design
+from pinjected.run_config_utils import load_variable_from_script
 
 
 def run_injected(
@@ -28,7 +30,8 @@ def run_injected(
         **kwargs
 ):
     logger.info(
-        f"run_injected called with cmd:{cmd}, var_path:{var_path}, design_path:{design_path}, args:{args}, kwargs:{kwargs}")
+        f"run_injected called with cmd:{cmd}, var_path:{var_path}, design_path:{design_path}, args:{args}, kwargs:{kwargs}"
+    )
     # TODO refactor to merge run_injected and run_anything
     with disable_internal_logging():
         if "return_result" in kwargs:
@@ -42,15 +45,17 @@ def run_injected(
         if design_path is None:
             design_path = get_design_path_from_var_path(var_path)
         logger.info(
-            f"run_injected called with cmd:{cmd}, var_path:{var_path}, design_path:{design_path}, args:{args}, kwargs:{kwargs}")
-    return run_anything(cmd,
-                        var_path,
-                        design_path=design_path,
-                        return_result=return_result,
-                        overrides=overrides,
-                        call_args=args,
-                        call_kwargs=kwargs,
-                        )
+            f"run_injected called with cmd:{cmd}, var_path:{var_path}, design_path:{design_path}, args:{args}, kwargs:{kwargs}"
+        )
+    return run_anything(
+        cmd,
+        var_path,
+        design_path=design_path,
+        return_result=return_result,
+        overrides=overrides,
+        call_args=args,
+        call_kwargs=kwargs,
+    )
 
 
 def run_anything(
@@ -75,13 +80,22 @@ def run_anything(
         meta_cxt: MetaContext = MetaContext.gather_from_path(ModuleVarPath(var_path).module_file_path)
         if design_path is None:
             design_path = meta_cxt.accumulated.provide("default_design_paths")[0]
-
-        var: Injected = Injected.ensure_injected(load_variable_by_module_path(var_path))
+        # here, actually the loaded variable maybe an instance of Designed.
+        # but it can also be a DelegatedVar[Designed] or a DelegatedVar[Injected] hmm,
+        # what would be the operation between Designed + Designed? run them on separate process, or in the same session?
         design: Design = load_variable_by_module_path(design_path)
+        match (var := load_variable_by_module_path(var_path)):
+            case Injected() | DelegatedVar():
+                var = Injected.ensure_injected(var)
+            case Designed():
+                design: Design = load_variable_by_module_path(design_path)
+                design += var.design
+                var = var.internal_injected
+
         meta_design = instances(overrides=instances()) + meta_cxt.accumulated
         overrides += meta_design.provide("overrides")
 
-    design = design + overrides
+    design += overrides
     logger.info(f"running target:{var} with {design_path} + {overrides}")
     logger.debug(design.keys())
     # logger.info(f"running target:{var} with cmd {cmd}, args {args}, kwargs {kwargs}")
@@ -103,6 +117,7 @@ def run_anything(
             if isinstance(res, Awaitable):
                 async def impl():
                     return await res
+
                 logger.info(f"awaiting awaitable")
                 res = asyncio.run(impl())
         elif cmd == 'get':
@@ -113,6 +128,7 @@ def run_anything(
             if isinstance(res, Awaitable):
                 async def impl():
                     return await res
+
                 logger.info(f"awaiting awaitable")
                 res = asyncio.run(impl())
             if not return_result:
@@ -123,12 +139,14 @@ def run_anything(
             if isinstance(res, Coroutine):
                 res = asyncio.run(res)
             logger.info(f"run_injected fire result:\n{res}")
-            if inspect.iscoroutinefunction(res) or (hasattr(res,'__is_async__') and res.__is_async__):
+            if inspect.iscoroutinefunction(res) or (hasattr(res, '__is_async__') and res.__is_async__):
                 logger.info(f'{res} is a coroutine function, wrapping it with asyncio.run')
                 src = res
-                #@wraps(res)
+
+                # @wraps(res)
                 def synced(*args, **kwargs):
                     return asyncio.run(src(*args, **kwargs))
+
                 res = synced
             else:
                 logger.info(f"{res} is not a coroutine function.")
@@ -155,3 +173,66 @@ def run_anything(
     notify(f"Run result:\n{str(res)[:100]}")
     if return_result:
         return res
+
+
+def find_dot_pinjected():
+    # 1. load the .pinjected.py at home directory
+    # 2. load the .pinjected.py at current directory
+    # hmm, we are currently only looking at the home and current directory.
+    home_dot_pinjected = Path("~/.pinjected.py").expanduser().absolute()
+    current_dot_pinjected = Path(".pinjected.py").absolute()
+    return home_dot_pinjected, current_dot_pinjected
+
+
+def load_design_from_paths(paths, design_name) -> Design:
+    res = instances()
+    for path in paths:
+        if path.exists():
+            logger.info(f"loading design from {path}:{design_name}.")
+            res += load_variable_from_script(path, design_name)
+        else:
+            logger.debug(f"design file {path} does not exist.")
+    return res
+
+
+def load_user_default_design():
+    """
+    This function loads user specific environment data from a python file.
+    the syntax is :
+    /path/to/python/file.py:design_variable_name
+    example:
+    /home/user/design.py:my_design
+    :return:
+    """
+    design_path = os.environ.get('PINJECTED_DEFAULT_DESIGN_PATH', "")
+    return load_design_from_paths(find_dot_pinjected(), "default_design") + _load_design(design_path)
+
+
+def _load_design(design_path):
+    if design_path == "":
+        return Design()
+    pairs = design_path.split("|")
+    res = instances()
+    for pair in pairs:
+        if pair == "":
+            continue
+        script_path, var_name = pair.split(':')
+        design = load_variable_from_script(script_path, var_name)
+        if design is not None:
+            assert isinstance(design,
+                              Design), f"design loaded from {script_path}:{var_name} is not a Design instance, but is {type(design)}."
+            res += design
+    return res
+
+
+def load_user_overrides_design():
+    """
+    This function loads user specific environment data from a python file.
+    the syntax is :
+    /path/to/python/file.py:design_variable_name
+    example:
+    /home/user/design.py:my_design
+    :return:
+    """
+    design_path = os.environ.get('PINJECTED_OVERRIDE_DESIGN_PATH', "")
+    return load_design_from_paths(find_dot_pinjected(), "overrides_design") + _load_design(design_path)
