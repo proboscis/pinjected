ed4228e604f70cf68502c5343d5d47840ab8836d
proboscis <nameissoap@gmail.com>
2024-03-19 14:40:22 +0900

    fix lambda provider issue

--- test/test_run_config_utils.py
+++ test/test_run_config_utils.py
@@ -1,18 +1,30 @@
+import sys
 from pathlib import Path
 
 from pinjected import Injected
 from pinjected.di.util import instances
+from pinjected.helper_structure import MetaContext
 from pinjected.ide_supports.create_configs import create_idea_configurations
 from pinjected.run_helpers.run_injected import run_injected
-
-TEST_MODULE = Path("../pinject_design/test_package/child/module1.py")
+from loguru import logger
+TEST_MODULE = Path("../pinjected/test_package/child/module1.py")
 
 
 def test_create_configurations():
-    create_idea_configurations(
+    configs = create_idea_configurations(
         TEST_MODULE.expanduser(),
         default_design_path="dummy_path"
     )
+    logger.info(configs)
+    mc = MetaContext.gather_from_path('../pinjected/ide_supports/create_configs.py')
+    (mc.final_design + instances(
+        # print_to_stdout=True,
+        module_path=TEST_MODULE,
+        # interpreter_path=sys.executable,
+        # meta_context = mc,
+        # logger = logger,
+        # runner_script_path= Path(__file__),
+    )).provide(configs)
 
 
 test_design = instances(x=0)
