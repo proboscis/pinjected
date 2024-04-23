8e4d7b08db69c00292e92bd3e8078f6d374a70e5
proboscis <nameissoap@gmail.com>
2024-04-23 17:37:34 +0900

    now create_idea_configurations uses pinjected.EmptyDesign in case none was specified in __meta_design__

--- pinjected/ide_supports/create_configs.py
+++ pinjected/ide_supports/create_configs.py
@@ -47,7 +47,8 @@ def run_with_meta_context(
     instance_overrides = instances(
         module_path=context_module_file_path,
         interpreter_path=sys.executable,
-        meta_context=meta_context
+        meta_context=meta_context,
+        default_design_paths = []
     ) + instances(**kwargs)
     return run_injected("get", var_path, design_path, return_result=True,
                         overrides=meta_context.accumulated + instance_overrides,
