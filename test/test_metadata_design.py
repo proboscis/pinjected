32d8b878eb8bab7516866475dfc5e0b1e0a8ec45
proboscis <nameissoap@gmail.com>
2024-06-11 18:48:03 +0900

    AddSummary and AddTags are implemented. user can add arbitrary class that implements 'Design' interface and later retrieve it.

---
+++ test/test_metadata_design.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from pinjected import instances, Design
+from pinjected.di.design import AddSummary, AddTags
+from loguru import logger
+
+from pinjected.v2.resolver import AsyncResolver
+
+
+def test_add_metadata():
+    d = instances()
+    d += AddSummary("This is a test")
+    d += AddTags("tag1")
+
+    def get_metadata(__design__:Design):
+        for d in __design__.dfs_design():
+            logger.info(d)
+
+    asyncio.run(AsyncResolver(d).provide(get_metadata))
