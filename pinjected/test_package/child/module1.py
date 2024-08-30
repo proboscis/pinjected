cbee6bff07e34abb2c1427af0cda44f6c2b5ed01
proboscis <nameissoap@gmail.com>
2024-08-30 19:16:21 +0900

    core of testing is implemented. now i need to implement the interface

--- pinjected/test_package/child/module1.py
+++ pinjected/test_package/child/module1.py
@@ -1,21 +1,12 @@
 from pinjected import *
+from pinjected.di.design_interface import DESIGN_OVERRIDES_STORE
 from pinjected.di.util import instances, providers
-
-__meta_design__ = instances(
-    name="test_package.child.module1",
-    default_design_paths=[
-        "pinjected.test_package.child.module1.design01"
-    ]
-)
 
 design01 = instances(name='design01')
 design02 = design01 + instances(name='design02')
 a = Injected.pure('a')
 b = Injected.pure('b')
 
-
-def injected(f):
-    return f
 
 
 @instance
@@ -32,13 +23,36 @@ def test_function(a, b):
 def test_function2(a, /, b):
     pass
 
-variable_x:IProxy = injected('x')
-variable_y:Injected = injected('y')
 
-
+variable_x: IProxy = injected('x')
+variable_y: Injected = injected('y')
 
 viz_target_design = providers(
     a=a,
     b=b
 )
 test_runnable = Injected.pure("hello world")
+
+@instance
+def test_always_failure():
+    raise RuntimeError("This is always failure")
+
+with design(
+        c="c"
+):
+    test_c = injected('c')
+    with design(
+        c="cc"
+    ):
+        test_cc = injected('c')
+
+__meta_design__ = instances(
+    overrides=design(
+        a="a",
+        b="b"
+    ),
+    name="test_package.child.module1",
+    default_design_paths=[
+        "pinjected.test_package.child.module1.design01"
+    ]
+)
