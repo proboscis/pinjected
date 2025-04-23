a43152129fcfeac2f8254334efc770a263469b48
proboscis <nameissoap@gmail.com>
2025-04-23 15:05:15 +0900

    fix

--- packages/mlflow/src/pinjected_mlflow/util.py
+++ packages/mlflow/src/pinjected_mlflow/util.py
@@ -5,21 +5,23 @@ from pathlib import Path
 from random import randint, random
 from threading import Event
 
+import loguru
 import mlflow
 from google.auth import default as google_auth_default, impersonated_credentials
 from google.auth.transport.requests import Request as AuthRequest
 from mlflow import ActiveRun
 
-from pinjected import instance
+from pinjected import instance, injected, design
 
 """
 I believe that the reason why this works on the job is that
 job has some GCP SA in the environment
 """
 
-# pinjected-reviewer: ignore
 
-def authorize_mlflow(mlflow_client_id) -> None:
+# pinjected-reviewer: ignore
+@injected
+def authorize_mlflow(logger, mlflow_client_id, /) -> None:
     """Create token to access ailab-mlflow via OAuth Identification
     NOTE:
     The expiration is 1 hour so that you MUST re-create access token regularly.
@@ -44,15 +46,16 @@ def authorize_mlflow(mlflow_client_id) -> None:
     id_token.refresh(AuthRequest())
     environ["MLFLOW_TRACKING_TOKEN"] = id_token.token
     assert environ["MLFLOW_TRACKING_TOKEN"] is not None, "Failed to create token"
+    logger.success("Successfully created token for mlflow")
 
 
 @instance
-async def mlflow_authentication_session(mlflow_client_id: str) -> None:
+async def mlflow_authentication_session(authorize_mlflow) -> None:
     authenticated = Event()
 
     def task():
         while True:
-            authorize_mlflow(mlflow_client_id)
+            authorize_mlflow()
             authenticated.set()
             time.sleep(30 * 60)
 
@@ -72,5 +75,58 @@ async def mlflow(mlflow_authentication_session, mlflow_tracking_uri):
 
 @instance
 def _test_log_mlflow(mlflow):
+    mlflow.set_experiment("test_experiment")
     with mlflow.start_run():
         mlflow.log_metric("accuracy", random(), step=1)
+
+
+@instance
+def _test_log_artifact(mlflow):
+    mlflow.set_experiment("test_experiment")
+    with mlflow.start_run():
+        import tempfile
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with open(file_path := (Path(tmpdir) / "test.txt"), "w") as f:
+                f.write("test")
+            mlflow.log_artifact(file_path)
+
+
+@instance
+def _test_use_artifact(logger, mlflow):
+    logger.info("Downloading artifact")
+    mlflow.artifacts.download_artifacts(
+        artifact_path="test.txt",
+        run_id="09163536d18f4a70a2e1a0abb12c7aba",
+        dst_path="tmp"
+    )
+    logger.success("Successfully downloaded")
+
+
+@instance
+def _test_log_large_artifact(logger,mlflow):
+    mlflow.set_experiment("test_experiment")
+    with mlflow.start_run():
+        import tempfile
+        with tempfile.TemporaryDirectory() as tmpdir:
+            logger.info(f"Creating large artifact in {tmpdir}")
+            with open(file_path := (Path(tmpdir) / "test.txt"), "w") as f:
+                f.write("test" * 1024 * 1024 * 1024) # 1GB
+            logger.info(f"uploading {file_path}")
+            mlflow.log_artifact(file_path)
+            logger.info("upload done?")
+
+@instance
+def _test_use_large_artifact(logger, mlflow):
+    logger.info("Downloading artifact")
+    # I think no SAM check is done during download...
+    mlflow.artifacts.download_artifacts(
+        artifact_path="test.txt",
+        run_id="dd3e4ff65ccd47868ba6465d926680c0",
+        dst_path="tmp"
+    )
+    logger.success("Successfully downloaded")
+
+
+__design__ = design(
+    logger=loguru.logger
+)
