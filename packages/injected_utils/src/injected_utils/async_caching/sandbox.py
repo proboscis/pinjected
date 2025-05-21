add8c533b1ff7d9634446fa654f55d7605e711cd
proboscis <nameissoap@gmail.com>
2025-05-21 12:22:09 +0900

    fix default key to param

--- packages/injected_utils/src/injected_utils/async_caching/sandbox.py
+++ packages/injected_utils/src/injected_utils/async_caching/sandbox.py
@@ -1,11 +1,11 @@
 from injected_utils import async_cached_v2
 from injected_utils.async_caching.async_cache import async_lzma_sqlite
-from pinjected import Injected, IProxy, injected
+from pinjected import Injected, IProxy, injected, design
 
 
 @async_cached_v2(cache=async_lzma_sqlite("test.db"))
 @injected
-async def a_test_func(a: int, b: int) -> int:
+async def a_test_func(x:int,/,a: int, b: int) -> int:
     return a + b
 
 
@@ -15,3 +15,6 @@ test_a_test_func: IProxy = Injected.tuple(
     a_test_func(0, 1),
     a_test_func(0, 1),
 )
+from loguru import logger
+
+__design__ = design(x=0,logger=logger)
