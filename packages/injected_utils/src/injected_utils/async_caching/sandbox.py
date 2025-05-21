3f559f7cdce70a09625b01a4516fc043a8ae0481
proboscis <nameissoap@gmail.com>
2025-05-21 11:52:06 +0900

    ruff ignore

---
+++ packages/injected_utils/src/injected_utils/async_caching/sandbox.py
@@ -0,0 +1,17 @@
+from injected_utils import async_cached_v2
+from injected_utils.async_caching.async_cache import async_lzma_sqlite
+from pinjected import Injected, IProxy, injected
+
+
+@async_cached_v2(cache=async_lzma_sqlite("test.db"))
+@injected
+async def a_test_func(a: int, b: int) -> int:
+    return a + b
+
+
+test_call_a_test_func: IProxy = a_test_func(0, 1)
+test_a_test_func: IProxy = Injected.tuple(
+    a_test_func(0, 1),
+    a_test_func(0, 1),
+    a_test_func(0, 1),
+)
